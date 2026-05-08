8411db1750125ad82b304fa041c8c6f1b311aadb
Andrew-Sedric <drvreen@gmail.com>
2026-05-08 04:13:16 +0300

    Update
    It includes the log in page

---
+++ api/auth.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$servername = getenv('DB_HOST');
+$username   = getenv('DB_USER');
+$password   = getenv('DB_PASSWORD');
+$dbname     = getenv('DB_NAME');
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); 
+$conn->real_connect($servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = mysqli_real_escape_string($conn, $_POST['username']);
+    $pass = mysqli_real_escape_string($conn, $_POST['password']);
+
+    // Check database for this user
+    $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass' LIMIT 1";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['admin_logged_in'] = true;
+        header("Location: ../admin.php"); // Successful login
+    } else {
+        echo "<script>alert('Wrong username or password!'); window.location.href='../login.php';</script>";
+    }
+}
+?>
